78b3ee140a5321b8be617e2a552504652307bd2a
Lagat100 <weldonkiprop1@gmail.com>
2024-02-09 00:59:37 +0300

    how to use **kwargs

--- fullpractice/*kwargs.py
+++ fullpractice/*kwargs.py
@@ -11,3 +11,16 @@ def print_values(**kwargs):
         print("The value of {} is {}".format(key, value))
 
 print_values(my_name="Sammy", your_name="Casey")
+
+def print_values(**kwargs):
+    for key, value in kwargs.items():
+        print("The value of {} is {}".format(key, value))
+
+print_values(
+            name_1="Alex",
+            name_2="Gray",
+            name_3="Harper",
+            name_4="Phoenix",
+            name_5="Remy",
+            name_6="Val"
+        )
