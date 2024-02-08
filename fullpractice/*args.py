34080d4ddc7e2761c07613e80a569ac1cc599c3a
Lagat100 <weldonkiprop1@gmail.com>
2024-02-09 01:07:11 +0300

    using *args to pass arguments into functions

--- fullpractice/*args.py
+++ fullpractice/*args.py
@@ -15,3 +15,12 @@ multiply(4, 5)
 multiply(10, 9)
 multiply(2, 3, 4)
 multiply(3, 5, 10, 6)
+
+def some_args(arg_1, arg_2, arg_3):
+    """ Here we are using *args to pass arguments into functions """
+    print("arg_1:", arg_1)
+    print("arg_2:", arg_2)
+    print("arg_3:", arg_3)
+
+args = ("Sammy", "Casey", "Alex")
+some_args(*args)
