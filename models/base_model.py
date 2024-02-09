57825e51142aa5d04e9d81302983d7fc5e425268
Lagat100 <weldonkiprop1@gmail.com>
2024-02-10 00:52:42 +0300

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 import uuid
 from datetime import datetime
-
 import models
 
 class BaseModel:
@@ -22,13 +21,14 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    setattr(self, key, datetime.strptime(kwargs, '%Y-%m-%dT%H:%M:%S.%f'))
                 else:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
